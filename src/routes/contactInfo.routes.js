ab26631794f8056db9d1b5e94b64a752645cd90f
Abu Taleb <abutaleb142@gmail.com>
2026-02-18 18:35:01 +0600

    updated

--- src/routes/contactInfo.routes.js
+++ src/routes/contactInfo.routes.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const controller = require("../controllers/contactInfo.controller");
 const publicController = require("../controllers/contactPublic.controller");
 
+// 1. Specific routes first
+router.get("/public", publicController.getPublicContact);
+
+// 2. Generic/Root routes last
 router.get("/", controller.get);
 router.post("/", controller.save);
-router.put("/", controller.save); // single-record system
-router.get("/public", publicController.getPublicContact);
+router.put("/", controller.save);
 
 module.exports = router;
